55ffea8e23ba55cb3b940ca29a6874c8d49c28c0
DJEAKE <djeakee@gmail.com>
2023-09-14 00:11:44 +0800

    Written programm

--- Calculator/Calculator/Calculator.cpp
+++ Calculator/Calculator/Calculator.cpp
@@ -1,6 +1,40 @@
 ﻿#include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "russian");
+
+	std::string userChoiseOperations = "";
+	float firstUserNumber;
+	float secondUserNumber;
+
+	std::cout << "Выберите операцию нажав на соотвестующий символ: \n[+] - сумма, [-] - разность," <<
+		"\n[*] - произведение, [/] - частное" << std::endl;
+	std::cin >> userChoiseOperations;
+
+	std::cout << "Введите первое число" << std::endl;
+	std::cin >> firstUserNumber;
+
+	std::cout << "Введите второе число" << std::endl;
+	std::cin >> secondUserNumber;
+
+	if (userChoiseOperations == "+")
+		std::cout << "Результат: " << firstUserNumber + secondUserNumber;
+	else if (userChoiseOperations == "-")
+	{
+		std::cout << "Результат: " << firstUserNumber - secondUserNumber;
+	}
+	else if (userChoiseOperations == "*")
+	{
+		std::cout << "Результат: " << firstUserNumber * secondUserNumber;
+	}
+	else if (userChoiseOperations == "/")
+	{
+		std::cout << "Результат: " << firstUserNumber / secondUserNumber;
+	}
+	else
+	{
+		std::cout << "ОШИБКА!";
+	}
 }
